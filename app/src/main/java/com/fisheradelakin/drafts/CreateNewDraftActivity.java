9c8994d085ce91bc2fe08d87050841581a1208f9
Fisher Adelakin <temidayoadelakin@gmail.com>
2016-01-08 02:35:45 -0500

    edge cases

--- app/src/main/java/com/fisheradelakin/drafts/CreateNewDraftActivity.java
+++ app/src/main/java/com/fisheradelakin/drafts/CreateNewDraftActivity.java
@@ -6,9 +6,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.afollestad.materialdialogs.DialogAction;
+import com.afollestad.materialdialogs.MaterialDialog;
 import com.fisheradelakin.drafts.db.ThoughtsDataSource;
 import com.fisheradelakin.drafts.model.Thought;
 
@@ -34,6 +37,45 @@ public class CreateNewDraftActivity extends AppCompatActivity {
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String thoughtString = draft.getText().toString();
+                if(thoughtString.isEmpty() || thoughtString.trim().length() == 0) {
+                    finish();
+                } else {
+                    new MaterialDialog.Builder(CreateNewDraftActivity.this)
+                            .content("Save draft?")
+                            .contentColor(getResources().getColor(R.color.colorAccent))
+                            .positiveText("Save")
+                            .onPositive(new MaterialDialog.SingleButtonCallback() {
+                                @Override
+                                public void onClick(MaterialDialog dialog, DialogAction which) {
+                                    Thought thought = new Thought();
+                                    thought.setDrafts(thoughtString);
+
+                                    mDataSource.createThought(thought);
+
+                                    Toast.makeText(CreateNewDraftActivity.this, "Draft saved.", Toast.LENGTH_SHORT).show();
+
+                                    Intent intent = new Intent(CreateNewDraftActivity.this, MainActivity.class);
+                                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                    startActivity(intent);
+                                }
+                            })
+                            .negativeText("Delete")
+                            .onNegative(new MaterialDialog.SingleButtonCallback() {
+                                @Override
+                                public void onClick(MaterialDialog dialog, DialogAction which) {
+                                    finish();
+                                }
+                            })
+                            .build().show();
+                }
+            }
+        });
+
         mDataSource = new ThoughtsDataSource(this);
     }
 
@@ -55,17 +97,35 @@ public class CreateNewDraftActivity extends AppCompatActivity {
         if (id == R.id.action_save) {
             String thoughtString = draft.getText().toString();
 
-            Thought thought = new Thought();
-            thought.setDrafts(thoughtString);
+            if(thoughtString.isEmpty() || thoughtString.trim().length() == 0) {
+                new MaterialDialog.Builder(this)
+                        .content("Save draft?")
+                        .contentColor(getResources().getColor(R.color.colorAccent))
+                        .positiveText("Delete")
+                        .onPositive(new MaterialDialog.SingleButtonCallback() {
+                            @Override
+                            public void onClick(MaterialDialog dialog, DialogAction which) {
+                                Intent intent = new Intent(CreateNewDraftActivity.this, MainActivity.class);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                startActivity(intent);
+                            }
+                        })
+                        .neutralText("Cancel")
+                        .build().show();
+            } else {
+                Thought thought = new Thought();
+                thought.setDrafts(thoughtString);
 
-            mDataSource.createThought(thought);
+                mDataSource.createThought(thought);
 
-            Toast.makeText(this, "Draft saved.", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "Draft saved.", Toast.LENGTH_SHORT).show();
 
-            Intent intent = new Intent(this, MainActivity.class);
-            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
-            startActivity(intent);
+                Intent intent = new Intent(this, MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+            }
         }
 
         return super.onOptionsItemSelected(item);
