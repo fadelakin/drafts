a03ec3592db21505d0563cb5921145392a66d97d
Fisher Adelakin <temidayoadelakin@gmail.com>
2016-09-08 11:18:06 -0400

    update a few things

--- app/src/main/java/com/fisheradelakin/drafts/MainActivity.java
+++ app/src/main/java/com/fisheradelakin/drafts/MainActivity.java
@@ -6,20 +6,17 @@ import android.content.Loader;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 import com.fisheradelakin.drafts.db.ThoughtsDataSource;
 import com.fisheradelakin.drafts.db.ThoughtsLoader;
-import com.fisheradelakin.drafts.model.Thought;
-
-import java.util.List;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -27,6 +24,7 @@ import butterknife.ButterKnife;
 public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
 
     @Bind(R.id.drafts_rv) RecyclerView draftsRV;
+    @Bind(R.id.empty_tv) TextView emptyTV;
 
     private ThoughtsDataSource mDataSource;
 
@@ -91,7 +89,14 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
 
     @Override
     public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
-        draftsRV.setAdapter(new DraftsRVAdapter(this, mDataSource.cursorToThoughts(data)));
+
+        if (mDataSource.cursorToThoughts(data).size() == 0) {
+            emptyTV.setVisibility(View.VISIBLE);
+            draftsRV.setVisibility(View.INVISIBLE);
+        } else {
+            draftsRV.setAdapter(new DraftsRVAdapter(this, mDataSource.cursorToThoughts(data)));
+            emptyTV.setVisibility(View.INVISIBLE);
+        }
     }
 
     @Override
